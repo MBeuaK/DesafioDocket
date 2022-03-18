d1854bc2dd17b8280a0efba2266f12f2d114d126
Kaue <kauemachado@frwk.com.br>
2022-03-18 08:28:04 -0300

    Ajustes no projeto

--- src/main/java/com/desafio/docket/controller/ApiController.java
+++ src/main/java/com/desafio/docket/controller/ApiController.java
@@ -1,11 +1,11 @@
 package com.desafio.docket.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class ApiController {
 
     @GetMapping
